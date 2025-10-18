a2c3904b373d46759b0c17189c6270a6b2c5b059
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-10-19 03:15:27 +0900

    fix(App): 공백 입력 처리 개선
    trim 함수를 사용하여 공백만 있는 입력도 빈 문자열 처리

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App {
   async run() {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
 
-    if (input === "") {
+    if (input.trim() === "") {
       Console.print("결과 : 0");
       return;
     }
