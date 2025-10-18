b6a77fb5a3e1e60a40292adf1b815da12e232c43
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-10-19 02:58:22 +0900

    feat(App): 커스텀 구분자 지원 기능 구현
    //로 시작하면 커스텀 구분자 추출해서 사용

--- src/App.js
+++ src/App.js
@@ -9,7 +9,17 @@ class App {
       return;
     }
 
-    const numbers = input.split(/[,:]/);
+    let delimiter = /[,:]/;
+    let numberString = input;
+
+    if (input.startsWith("//")) {
+      const delimiterEndIndex = input.indexOf("\\n");
+      const customDelimiter = input.substring(2, delimiterEndIndex);
+      delimiter = new RegExp(`[,:${customDelimiter}]`);
+      numberString = input.substring(delimiterEndIndex + 2);
+    }
+
+    const numbers = numberString.split(delimiter);
     const sum = numbers.reduce((acc, num) => acc + Number(num), 0);
     Console.print(`결과 : ${sum}`);
   }
